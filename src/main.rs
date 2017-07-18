07f2b3e110c325a9e0969557762849f1b42b7732
Bastien Duplessier <bastien.duplessier@gmail.com>
2017-07-19 00:22:03 +0200

    Read stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome into the SUUS Shell.");
+    println!("Type anything you want. It may not work.");
+    main_loop();
+}
+
+fn main_loop() {
+    let mut line = String::new();
+    let _ = io::stdin().read_line(&mut line);
+    match line.trim_matches('\n') {
+        command => not_found(command)
+    }
+}
+
+
+fn not_found(command: &str) {
+    println!("Command {} not found", command);
+    main_loop()
 }
