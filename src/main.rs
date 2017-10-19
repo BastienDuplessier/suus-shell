16ba8dd8f0c2081fc4992e698c8c22ff6611ae1b
Bastien Duplessier <bastien.duplessier@gmail.com>
2017-10-19 22:27:49 +0200

    Refact command matching, include main_loop call inside main_loop

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,13 @@ fn main_loop() {
     let (command, args) = parse_input();
     match command.as_str() {
         "exit" => exit_loop(),
-        "echo" => echo(args),
-        command => not_found(command)
+        command => {
+            match command {
+                "echo" => echo(args),
+                cmd => not_found(cmd)
+            };
+            main_loop()
+        }
     }
 }
 
@@ -33,10 +38,8 @@ fn exit_loop() {
 
 fn not_found(command: &str) {
     println!("Command {} not found", command);
-    main_loop()
 }
 
 fn echo(text:String) {
     println!("{}", text);
-    main_loop()
 }
